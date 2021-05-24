7a07494d5626f9fd698f01129594c8c5059f7da7
tipotto <label1134@gmail.com>
2021-05-24 14:57:38 +0900

    Update cloudbuild.yaml

--- cloudbuild.sh
+++ cloudbuild.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
+# DIR=api
 echo "Build process started..."
 echo PORT=8080 >> .env
-echo PROD_PYTHON_SCRIPT="~/markets/app/python/index.py" >> .env
-mkdir markets
-mv *[^markets] ./markets
+echo PROD_PYTHON_SCRIPT="~/api/app/python/index.py" >> .env
+# .env > /workspace/
+mkdir api
+mv .env ./api
+mv *[^api] ./api
+ls -l
 # ls *[^markets] | xargs mv -t ./markets
 # chown -R tipotto404:tipotto404 ./markets
 # cp -r *[^markets] ./markets
