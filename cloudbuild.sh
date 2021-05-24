c272da098992dfaa622bf2e5ba50581c090f5e37
tipotto <label1134@gmail.com>
2021-05-24 12:38:16 +0900

    Update cloudbuild.sh

--- cloudbuild.sh
+++ cloudbuild.sh
@@ -6,6 +6,6 @@ echo PROD_PYTHON_SCRIPT="~/markets/app/python/index.py" >> .env
 mkdir markets
 # mv *[^markets] ./markets
 ls *[^markets] | xargs mv -t ./markets
-chown -R tipotto404:tipotto404 ./markets
+# chown -R tipotto404:tipotto404 ./markets
 # cp -r *[^markets] ./markets
 # cp -r ./* ./markets
