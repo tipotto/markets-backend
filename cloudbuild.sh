95b6040f7b05bbbd15393ad1cba4128d295c17eb
tipotto <label1134@gmail.com>
2021-05-24 12:11:18 +0900

    Update cloudbuild.sh

--- cloudbuild.sh
+++ cloudbuild.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+echo "Build process started..."
 echo PORT=8080 > .env
 echo PROD_PYTHON_SCRIPT="~/markets/app/python/index.py" >> .env
+mkdir markets
+mv *[^markets] ./markets
+# cp -r *[^markets] ./markets
+# cp -r ./* ./markets
